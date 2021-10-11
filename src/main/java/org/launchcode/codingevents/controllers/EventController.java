703c655c3d91fcfcf458d2dd97694d20e2df28cc
swankidelic <swankidelic@gmail.com>
2021-10-11 00:47:59 -0500

    chapter exercise complete - proud that I figured out how to loop a HashMap to generate table rows

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,16 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        events.put("WWDC", "Single-minded zealots awaiting instructions from the mothership");
+        events.put("SpringLaunchBootCode", "Literally every front-end technology, ever");
+        events.put("Lazy Larry's Swap & Shop Shack", "Only for the OG devs");
+
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -30,8 +35,9 @@ public class EventController {
 
     // URL /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, String eventDesc) {
+        events.put(eventName, eventDesc);
+
         return "redirect:";
     }
 }
