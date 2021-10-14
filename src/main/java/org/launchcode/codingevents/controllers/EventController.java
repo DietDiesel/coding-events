684bf7d94438fa0605c1f31bd7fca17dd4d44dd2
swankidelic <swankidelic@gmail.com>
2021-10-14 00:07:35 -0500

    completed 12.3.6

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,11 +16,9 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -33,7 +32,26 @@ public class EventController {
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName,
                               @RequestParam String eventDesc) {
-        events.add(new Event(eventName, eventDesc));
+        EventData.add(new Event(eventName, eventDesc));
+        return "redirect:";
+    }
+
+    // URL /events/delete
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String deleteEvent(@RequestParam int[] eventIds) {
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+
         return "redirect:";
     }
 }
